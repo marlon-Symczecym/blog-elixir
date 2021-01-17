0ccc7f0353ac78919da5038b4b99cef5ba198da8
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-17 16:16:02 -0300

    ref: file deploy changed master to main

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Start Gigalixir Deploy"
 
 git remote add gigalixir https://marlonsymczecym.dev%40gmail.com:5e3a1e40-964d-4180-bbbc-4ffa94752421@git.gigalixir.com/blogelixir.git
-git push gigalixir master
+git push gigalixir main
 
 echo "Finish Gigalixir Deploy"
