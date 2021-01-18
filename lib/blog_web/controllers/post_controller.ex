73ee37fcca1a209e35e574228ac46d283e1111da
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-17 23:24:30 -0300

    feat: posts index and show

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -5,5 +5,25 @@ defmodule BlogWeb.PostController do
   use BlogWeb, :controller
 
   def index(conn, params) do
+    posts = [
+      %{
+        id: 1,
+        title: "Phoenix"
+      },
+      %{
+        id: 2,
+        title: "LiveView"
+      },
+      %{
+        id: 3,
+        title: "PostgreSQL"
+      }
+    ]
+
+    render(conn, "index.html", posts: posts)
+  end
+
+  def show(conn, params) do
+    render(conn, "show.html")
   end
 end
