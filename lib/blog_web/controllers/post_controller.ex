2b38569d61de8de2584d8c3ab0c674ca50f873df
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-19 20:48:14 -0300

    feat: Post schema, migration and seeds

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -4,26 +4,17 @@ defmodule BlogWeb.PostController do
   """
   use BlogWeb, :controller
 
+  alias Blog.Posts.Post
+
   def index(conn, params) do
-    posts = [
-      %{
-        id: 1,
-        title: "Phoenix"
-      },
-      %{
-        id: 2,
-        title: "LiveView"
-      },
-      %{
-        id: 3,
-        title: "PostgreSQL"
-      }
-    ]
+    posts = Blog.Repo.all(Post)
 
     render(conn, "index.html", posts: posts)
   end
 
-  def show(conn, params) do
-    render(conn, "show.html")
+  def show(conn, %{"id" => id}) do
+    post = Blog.Repo.get(Post, id)
+
+    render(conn, "show.html", post: post)
   end
 end
