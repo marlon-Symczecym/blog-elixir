794c2c4d90985dab47b41d46b9d22f3c1e839536
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-22 19:28:09 -0300

    ref: update to context

--- lib/blog_web/controllers/post_controller.ex
+++ lib/blog_web/controllers/post_controller.ex
@@ -4,10 +4,10 @@ defmodule BlogWeb.PostController do
   """
   use BlogWeb, :controller
 
-  alias Blog.Posts.Post
+  alias Blog.{Posts, Posts.Post}
 
   def index(conn, _params) do
-    posts = Blog.Repo.all(Post)
+    posts = Posts.list_posts()
 
     render(conn, "index.html", posts: posts)
   end
@@ -18,11 +18,7 @@ defmodule BlogWeb.PostController do
   end
 
   def create(conn, %{"post" => post}) do
-    post =
-      Post.changeset(%Post{}, post)
-      |> Blog.Repo.insert()
-
-    case post do
+    case Posts.create_post(post) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post criado com sucesso")
@@ -34,19 +30,13 @@ defmodule BlogWeb.PostController do
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Blog.Repo.get(Post, id)
+    post = Posts.get_post!(id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Blog.Repo.get!(Post, id)
-
-    changeset = Post.changeset(post, post_params)
-
-    post = Blog.Repo.update(changeset)
-
-    case post do
+    case Posts.update_post(id, post_params) do
       {:ok, post} ->
         conn
         |> put_flash(:info, "Post atualizado com sucesso")
@@ -58,9 +48,7 @@ defmodule BlogWeb.PostController do
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Blog.Repo.get(Post, id)
-
-    Blog.Repo.delete(post)
+    Posts.delete_post(id)
 
     conn
     |> put_flash(:info, "O post foi deletado")
@@ -68,7 +56,7 @@ defmodule BlogWeb.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = Blog.Repo.get(Post, id)
+    post = Posts.get_post!(id)
 
     render(conn, "show.html", post: post)
   end
