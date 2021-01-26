7d61a54505a1e51fafca574ca6207873d5e04ee9
marlon symczecym <marlonsymczecym.dev@gmail.com>
2021-01-25 22:19:02 -0300

    finalized the test module

--- test/blog/posts/post_test.exs
+++ test/blog/posts/post_test.exs
@@ -29,7 +29,7 @@ defmodule Blog.Posts.PostTest do
 
   test "update_post/2 with valid data" do
     post = post_fixture()
-    assert {:ok, %Post{} = post} = Posts.update_post(post.id, @update_post)
+    assert {:ok, %Post{} = post} = Posts.update_post(post, @update_post)
     assert post.title == "updated"
   end
 
